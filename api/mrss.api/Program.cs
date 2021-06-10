e140efa16ce0d3a7bdb2f67bd2504c0d10c5a4f4
Saurav Sahu <mrsauravsahu@outlook.com>
2021-06-10 09:04:42 +0530

    add version to root route

--- api/mrss.api/Program.cs
+++ api/mrss.api/Program.cs
@@ -1,13 +1,15 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 
 var app = WebApplication.Create();
 
+var version = new Version(File.ReadAllText("VERSION"));
 app.MapGet("/", new Func<dynamic>(() => new
 {
   Data = new
   {
-    App = "mrss"
+    Version = version
   }
 }));
 
