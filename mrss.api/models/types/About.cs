65e0ecf1c4ac5227a7ae3aafd4d9f35e1cc43700
Saurav Sahu <mrsauravsahu@outlook.com>
2021-06-12 19:11:36 +0530

    setup graphql with graphql-dotnet

---
+++ mrss.api/models/types/About.cs
@@ -0,0 +1,17 @@
+using GraphQL.Types;
+
+namespace mrss.api.models.types
+{
+
+
+  public class About : ObjectGraphType<About>
+  {
+    public new string Name { get; set; }
+    public string Version { get; set; }
+    public About()
+    {
+      Field(p => p.Name).Description("Name of the app");
+      Field(p => p.Version).Description("Version of the app");
+    }
+  }
+}
