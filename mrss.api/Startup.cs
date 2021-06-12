95abfa3fa8b3401426bf72200449d71adbc703d2
Saurav Sahu <mrsauravsahu@outlook.com>
2021-06-12 14:36:10 +0530

    Setup: use dotnet5 (#3)
    * create an api project with dotnet 5
    
    * move api project to dotnet 5
    
    * change dockerfile for dotnet 5; add compose

---
+++ mrss.api/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace mrss.api
+{
+  public class Startup
+  {
+    public Version AppVersion { get; set; }
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+      AppVersion = new Version(File.ReadAllText("./VERSION"));
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc(
+            $"v{AppVersion}",
+            new OpenApiInfo
+            {
+              Title = "mrss.api",
+              Version = $"v{AppVersion}"
+            });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint($"/swagger/v{AppVersion}/swagger.json", "mrss.api"));
+      }
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
